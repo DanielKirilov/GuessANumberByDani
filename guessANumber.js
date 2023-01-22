eca2344bdd4c68343d176bcc172e7bc4919021f4
DanielKirilov <dancata08@gmail.com>
2023-01-22 18:51:20 +0200

    Added levels to the game
    Added levels to make the game more difficult . The game now has 3 different levels.  Also added comments to explain some of the code.

--- guessANumber.js
+++ guessANumber.js
@@ -5,23 +5,26 @@ const readline = require("readline").createInterface({
 
 let computerGuess = Math.floor(Math.random() * 100)
 let guess;
- // this is the number that we will try to guess!
+let guessCount = 0;
+ // this is the number that we will try to guess
 
-let recursiveAsyncReadLine = function () {
-    readline.question("Guess the number from 1 - 100: ", number => {
+let recursiveAsyncReadLine = function () { // creating level 1
+    readline.question("Guess the number from 1 - 100: ", number => { // creating a question
         let guess = Number(number);
         if (100 >= guess && guess >= 0) {
-            if (guess === computerGuess) {
+            if (guess === computerGuess) { // if you guess correctly
                 console.log("That's right! It's too ez! You are the champion! Excellent !!!");
-                return readline.close();
-            } else if (guess > computerGuess) {
+                console.log("Congrats you have reached Level 2! Good Luck! ");
+                return recursiveAsyncReadLine2()
+                
+            } else if (guess > computerGuess) { // if the number is too high
                 console.log("Nah, that's too high :( Sorry, Try Again!");
                 recursiveAsyncReadLine()
-            } else if (guess < computerGuess) {
+            } else if (guess < computerGuess) { // if the number is too low
                 console.log("Nah, that's too low :( Sorry, Try Again!");
                 recursiveAsyncReadLine()
             } 
-        } else {
+        } else { // if its not in the range or NaN
             console.log("Wrong Input! Try Again!");
             recursiveAsyncReadLine()
         }
@@ -29,3 +32,54 @@ let recursiveAsyncReadLine = function () {
 } // this is a function to ask the question to guess the number
 recursiveAsyncReadLine()
 
+let computerGuess2 = Math.floor(Math.random() * 200)
+let guess2;
+
+let recursiveAsyncReadLine2 = function () { // creating level 2 
+    readline.question("Guess the number from 1 - 200: ", number => { // creating a question
+        let guess2 = Number(number);
+        if (200 >= guess2 && guess2 >= 0) {
+            if (guess2 === computerGuess2) { // if you guess correctly
+                console.log("That's right! It's too ez! You are the champion! Excellent !!!");
+                console.log("Congrats you have reached Level 3! Good Luck! ");
+                return recursiveAsyncReadLine3()
+            } else if (guess2 > computerGuess2) { // if the number is too high
+                console.log("Nah, that's too high :( Sorry, Try Again!");
+                recursiveAsyncReadLine2()
+            } else if (guess2 < computerGuess2) { // if the number is too low
+                console.log("Nah, that's too low :( Sorry, Try Again!");
+                recursiveAsyncReadLine2()
+            } 
+        } else { // if its not in the range or NaN
+            console.log("Wrong Input! Try Again!");
+            recursiveAsyncReadLine2()
+        }
+})
+} // this is a function to ask the question to guess the number
+recursiveAsyncReadLine2()
+
+let computerGuess3 = Math.floor(Math.random() * 300);
+let guess3;
+4
+let recursiveAsyncReadLine3 = function () { // creating level 3
+    readline.question("Guess the number from 1 - 300: ", number => { // creating a question
+        let guess3 = Number(number);
+        if (300 >= guess3 && guess3 >= 0) {
+            if (guess3 === computerGuess3) { // if you guess correctly
+                console.log("That's right! It's too ez! You are the champion! Excellent !!!");
+                console.log("You just won the game!! You are amazing!");
+                return readline.close();
+            } else if (guess3 > computerGuess3) { // if the number is too high
+                console.log("Nah, that's too high :( Sorry, Try Again!");
+                recursiveAsyncReadLine3()
+            } else if (guess3 < computerGuess3) { // if the number is too low
+                console.log("Nah, that's too low :( Sorry, Try Again!");
+                recursiveAsyncReadLine3()
+            } 
+        } else { // if its not in the range or NaN
+            console.log("Wrong Input! Try Again!");
+            recursiveAsyncReadLine3()
+        }
+})
+} // this is a function to ask the question to guess the number
+recursiveAsyncReadLine3()
